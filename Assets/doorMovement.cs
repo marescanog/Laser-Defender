dc3feab24ac6f0033d9ae9b18a91dafbcdd66cc8
Marvie Gasataya <marescanog@gmail.com>
2020-09-09 16:24:13 +0800

    Adjusted Door controller script to factor in the object movement

--- Assets/doorMovement.cs
+++ Assets/doorMovement.cs
@@ -4,33 +4,53 @@ using UnityEngine;
 
 public class doorMovement : MonoBehaviour
 {
-    Vector2 origDoorPos = new Vector2(0, 0);
-    Vector2 openedDoorPos = new Vector2(0, 0.8f);
-    Vector2 doorState;
+    readonly Vector3 openedDoorPos = new Vector3(0, 0.8f,0);
+    Vector3 doorState;
+    Vector3 doorOpen;
+    Vector3 doorClosed;
     [SerializeField] float doorSpeed = 1;
 
+    bool isDoorActivated = false;
+    bool isDoorOpen = false;
     // Start is called before the first frame update
     void Start()
     {
-        doorState = origDoorPos;
-
+        doorClosed = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.position = Vector2.MoveTowards(transform.position, doorState, doorSpeed * Time.deltaTime);
+        if (isDoorActivated)
+        {
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition, doorState, doorSpeed * Time.deltaTime);
+            if (transform.localPosition == doorState)
+            {
+                isDoorActivated = false;
+            }
+        }
+        doorOpen = transform.localPosition + openedDoorPos;
+        doorClosed = transform.localPosition - openedDoorPos;
     }
 
     public void GoUP()
     {
-        doorState = openedDoorPos;
+        if(!isDoorActivated & !isDoorOpen)
+        {
+            doorState = doorOpen;
+            isDoorActivated = true;
+            isDoorOpen = true;
+        }
     }
 
     public void GoDown()
     {
-        doorState = origDoorPos;
+        if (!isDoorActivated & isDoorOpen)
+        {
+            doorState = doorClosed;
+            isDoorActivated = true;
+            isDoorOpen = false;
+        }
     }
 
 }
