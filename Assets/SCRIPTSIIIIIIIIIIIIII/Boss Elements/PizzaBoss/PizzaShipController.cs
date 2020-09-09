af520b62ab404c3bb033ee3cbb9dfd7666d9667c
Marvie Gasataya <marescanog@gmail.com>
2020-09-09 14:11:49 +0800

    Added a controller script for the boss

---
+++ Assets/SCRIPTSIIIIIIIIIIIIII/Boss Elements/PizzaBoss/PizzaShipController.cs
@@ -0,0 +1,34 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaShipController : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    doorMovement _doorMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _doorMovement = door.GetComponent<doorMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ForDebugging
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _doorMovement.GoUP();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _doorMovement.GoDown();
+        }
+
+    }
+
+
+}
